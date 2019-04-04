754586f403b14c4b3cb985833d6e7d69058747ca
Craig Luedtke <craig.luedtke@renaissance.com>
2019-04-04 11:56:02 -0500

    configure and use ocelot

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Ocelot.Middleware;
+using Ocelot.DependencyInjection;
 
 namespace Gateway
 {
@@ -14,7 +16,21 @@ namespace Gateway
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args)
+                .ConfigureAppConfiguration((hostingContext, config) => {
+                    // informs the app to use the ocelot.json file
+                    config.AddJsonFile("ocelot.json");
+                })
+                .ConfigureServices(s => {
+                    // enable ocelot DI
+                    s.AddOcelot(); 
+                })
+                .Configure(app => {
+                    // include ocelot middleware
+                    app.UseOcelot().Wait();
+                })
+                .Build()
+                .Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-Adapted from https://www.pogsdotnet.com/2018/08/building-simple-api-gateways-with.html
+# ocelot-api-gateway
+
+Adapted from: https://www.pogsdotnet.com/2018/08/building-simple-api-gateways-with.html
+
+Demonstrates routing http requests using [Ocelot](https://ocelot.readthedocs.io/en/latest/).
+
+## Testiing Routing
+1. Run each project:
+   ```
+   dotnet run --project Authentication
+   dotnet run --project Catalog
+   dotnet run --project Gateway
+   dotnet run --project Ledger
+   ```
+
+2. Test each API gateway route defined in `ocelot.json`:
+   - Users Endpoint .. http://localhost:52793/api/users
+   - Products Endpoint .. http://localhost:52793/api/products
+   - Transactions Endpoint .. http://localhost:52793/api/transactions
